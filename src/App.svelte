0c845e7e1f0c8d06705b66c5ae846fba9ce07819
Alex Merced <alex@alexmerced.dev>
2021-08-15 21:49:39 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,101 @@
+<script>
+  import {onMount} from "svelte"
+  import Display from "./components/Display.svelte"
+  import Form from "./components/Form.svelte"
+
+  const url = "http://localhost:10000/todos"
+
+  //state
+  let showForm = false
+  let reminder = ""
+  let completed = false
+  let _id = ""
+  let todos = []
+  let action = "create"
+
+  // functions
+  const getTodos = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    todos = data
+  }
+
+  const toggleForm = () => {
+    showForm = !showForm
+  }
+
+  const resetState = () => {
+    reminder = ""
+    completed = false
+    showForm = false
+    _id = ""
+    action = "create"
+    getTodos()
+  }
+
+  const createTodos = async (todo) => {
+    await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(todo)
+    })
+    resetState()
+  }
+
+  const selectTodoToUpdate = (todo) => {
+    reminder = todo.reminder
+    completed = todo.completed
+    _id = todo._id
+    action = "update"
+    showForm = true
+  }
+
+  const updateTodos = async (todo) => {
+    await fetch(url + `/${todo._id}`, {
+      method: "put",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(todo)
+    })
+    resetState()
+  }
+
+  const deleteTodos = async (todo) => {
+    await fetch(url + `/${todo._id}`, {
+      method: "delete"
+    })
+    resetState()
+  }
+
+  // Lifecycle
+  onMount(() => { getTodos() })
+  
+
+</script>
+
+<main>
+  <h1>Alex's Todo List</h1>
+  <button on:click={toggleForm}>Create A Todo</button>
+  <Display 
+    todos={todos} 
+    select={selectTodoToUpdate}
+    destroy={deleteTodos}
+  />
+  {#if showForm}
+  <Form
+    reminder={reminder}
+    completed={completed}
+    _id={_id}
+    action={action}
+    create={createTodos}
+    update={updateTodos}
+  />
+  {/if}  
+</main>
+
+<style>
+
+</style>
